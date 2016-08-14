33e67c127b7f2781bc57a596276cd2317f8d8f31
Sebastian Thomschke <sebthom@users.noreply.github.com>
2016-08-14 23:01:19 +0200

    added pre-checks

--- publish-release.cmd
+++ publish-release.cmd
@@ -2,26 +2,46 @@
 :: @author Sebastian Thomschke, Vegard IT GmbH
 ::
 :: creates a new release in GitHub and haxelib.org
-::
-:: requires GITHUB_ACCESS_TOKEN environment variable to be set
-:: requires wget to be installed https://eternallybored.org/misc/wget/
-::
-setlocal
-set PROJECT_ROOT=%~dp0
-cd %PROJECT_ROOT%
-set ARTIFACT=haxe-strings
-set REPO=vegardit/%ARTIFACT%
-set DRAFT=false
+
+where  zip.exe /Q
+if %errorlevel% neq 0 (
+    echo Rerquired command 'zip' not found. Download from http://www.info-zip.org/Zip.html#Downloads
+    exit /b 1
+)
+
+where wget.exe /Q
+if %errorlevel% neq 0 (
+    echo Rerquired command 'wget' not found. Download from https://eternallybored.org/misc/wget/
+    exit /b 1
+)
+
+if [%GITHUB_ACCESS_TOKEN%] == [] (
+    echo Rerquired environment variable GITHUB_ACCESS_TOKEN is not set!
+    exit /b 1
+)
+
+setlocalset DRAFT=false
 set PREPRELEASE=false
 
-REM extract release version
-for /f "tokens=*" %%a in ( 'findstr version haxelib.json' ) do (set versionLine=%%a)
-set RELEASE_VERSION=%versionLine:"version": "=%
-set RELEASE_VERSION=%RELEASE_VERSION:",=%
+:: cd into project root
+cd %~dp0
 
-REM extract release note
-for /f "tokens=*" %%a in ( 'findstr releasenote haxelib.json' ) do (set releaseNoteLine=%%a)
-set RELEASE_NOTE=%releaseNoteLine:"releasenote": "=%
+REM extract GIT URL from haxelib.json
+for /f "tokens=*" %%a in ( 'findstr url haxelib.json' ) do (set textLine=%%a)
+set REPO_URL=%textLine:"name": "=%
+set REPO_URL=%REPO_URL:",=%
+
+REM extract repo name from haxelib.json
+set REPO_NAME=%REPO_URL:https://github.com/=%
+
+REM extract project version from haxelib.json
+for /f "tokens=*" %%a in ( 'findstr version haxelib.json' ) do (set textLine=%%a)
+set PROJECT_VERSION=%textLine:"version": "=%
+set PROJECT_VERSION=%PROJECT_VERSION:",=%
+
+REM extract release note from haxelib.json
+for /f "tokens=*" %%a in ( 'findstr releasenote haxelib.json' ) do (set textLine=%%a)
+set RELEASE_NOTE=%textLine:"releasenote": "=%
 set RELEASE_NOTE=%RELEASE_NOTE:",=%
 
 if not exist target mkdir target
@@ -29,15 +49,15 @@ if not exist target mkdir target
 :: create github release https://developer.github.com/v3/repos/releases/#create-a-release
 (
   echo {
-  echo "tag_name":"v%RELEASE_VERSION%",
-  echo "name":"v%RELEASE_VERSION%",
+  echo "tag_name":"v%PROJECT_VERSION%",
+  echo "name":"v%PROJECT_VERSION%",
   echo "target_commitish":"master",
   echo "body":"%RELEASE_NOTE%",
   echo "draft":%DRAFT%,
   echo "prerelease":%PREPRELEASE%
   echo }
 )>target/github_release.json
-wget -qO- --post-file=target/github_release.json "https://api.github.com/repos/%REPO%/releases?access_token=%GITHUB_ACCESS_TOKEN%" || goto :eof
+wget -qO- --post-file=target/github_release.json "https://api.github.com/repos/%REPO_NAME%/releases?access_token=%GITHUB_ACCESS_TOKEN%" || goto :eof
 
 :: create haxelib release
 zip target/haxelib-upload.zip src haxelib.json LICENSE.txt CONTRIBUTING.md README.md -r -9 || goto :eof
