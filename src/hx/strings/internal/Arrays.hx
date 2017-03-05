6221384a0aa467d34a9ce6d898fdf23c599c57bb
Sebastian Thomschke <sebthom@users.noreply.github.com>
2017-03-05 16:47:04 +0100

    cleanup

--- src/hx/strings/internal/Arrays.hx
+++ src/hx/strings/internal/Arrays.hx
@@ -26,22 +26,30 @@ class Arrays {
     
     /**
      * <pre><code>
+     * > >> Arrays.first(["1", "2"]) == "1"
+     * > >> Arrays.first([])         == null
+     * > >> Arrays.first(null)       == null
+     * </code></pre>
+     */
+    #if !hl
+    // see https://github.com/HaxeFoundation/haxe/issues/6071
+    inline
+    #end
+    public static function first<T>(items:Array<T>):T {
+        return (items == null || items.length == 0) ? null : items[0];
+    }
+    
+    /**
+     * <pre><code>
      * >>> Arrays.unique(["1", "1", "2"]) == ["1", "2"]
      * </code></pre>
      */
     public static function unique<T>(items:Array<T>):Array<T> {
-        var result = new Array<T>();
+        var filtered = new Array<T>();
+
         for (i in items) {
-            if (result.indexOf(i) == -1)
-                result.push(i);
+            if (filtered.indexOf(i) == -1) filtered.push(i);
         }
-        return result;
-    }
-    
-    #if !hl
-    inline
-    #end
-    public static function first<T>(items:Array<T>):T {
-        return (items == null || items.length == 0) ? null :items[0];
+        return filtered;
     }
 }
