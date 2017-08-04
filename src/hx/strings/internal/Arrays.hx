96261158045fe5c7da68cb34e0a6bff557afa882
Sebastian Thomschke <sebthom@users.noreply.github.com>
2017-08-04 22:08:58 +0200

    re-enable test cases

--- src/hx/strings/internal/Arrays.hx
+++ src/hx/strings/internal/Arrays.hx
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2016-2017 Vegard IT GmbH, http://vegardit.com
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,18 +17,18 @@ package hx.strings.internal;
 
 /**
  * <b>IMPORTANT:</b> This class it not part of the API. Direct usage is discouraged.
- * 
+ *
  * @author Sebastian Thomschke, Vegard IT GmbH
  */
 @:noDoc @:dox(hide)
 @:noCompletion
 class Arrays {
-    
+
     /**
      * <pre><code>
-     * > >> Arrays.first(["1", "2"]) == "1"
-     * > >> Arrays.first([])         == null
-     * > >> Arrays.first(null)       == null
+     * >>> Arrays.first(["1", "2"]) == "1"
+     * >>> Arrays.first([])         == null
+     * >>> Arrays.first(null)       == null
      * </code></pre>
      */
     #if !hl
@@ -38,7 +38,7 @@ class Arrays {
     public static function first<T>(items:Array<T>):T {
         return (items == null || items.length == 0) ? null : items[0];
     }
-    
+
     /**
      * <pre><code>
      * >>> Arrays.unique(["1", "1", "2"]) == ["1", "2"]
