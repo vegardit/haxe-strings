22f73c4f0b8b123681187c0d7b58465d45b8e0ab
Sebastian Thomschke <sebthom@users.noreply.github.com>
2018-11-26 11:53:14 +0100

    add tests

--- src/hx/strings/AnyAsString.hx
+++ src/hx/strings/AnyAsString.hx
@@ -5,17 +5,23 @@
 package hx.strings;
 
 /**
+ * <pre><code>
+ * >>> function(){var str:AnyAsString = 1;      return str; }() == "1"
+ * >>> function(){var str:AnyAsString = true;   return str; }() == "true"
+ * >>> function(){var str:AnyAsString = "cat";  return str; }() == "cat"
+ * >>> function(){var str:AnyAsString = [1, 2]; return str; }() == "[1,2]"
+ * </code></pre>
+ *
  * @author Sebastian Thomschke, Vegard IT GmbH
  */
 @:noDoc @:dox(hide)
 @:noCompletion
 abstract AnyAsString(String) from String to String {
+    @:from
+    inline
+    static function fromBool(value:Bool):AnyAsString return value ? "true" : "false";
 
-  @:from
-  inline
-  static function fromBool(value:Bool):AnyAsString return value ? "true" : "false";
-
-  @:from
-  inline
-  static function fromAny(value:Dynamic):AnyAsString return Std.string(value);
+    @:from
+    inline
+    static function fromAny(value:Dynamic):AnyAsString return Std.string(value);
 }
